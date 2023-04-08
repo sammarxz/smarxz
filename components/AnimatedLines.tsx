791b9d911abb2db251661fb5fbde715ce851dabc
Sam Marxz <sammarxzz@gmail.com>
2023-04-08 16:06:07 -0300

    create Header

---
+++ components/AnimatedLines.tsx
@@ -0,0 +1,87 @@
+import { useRef, useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+
+const lineAnim = {
+  initial: { 
+    y: 100,
+  },
+  animate: (i:number) => {
+    const delay = i * 0.1;
+    return {
+      y: 0,
+      transition: {
+        delay,
+        ease: [0.6, 0.01, -0.05, 0.95],
+        duration: 1.4,
+      }
+    }
+  },
+};
+
+type AnimatedLinesProps = {
+  delay: number
+  children: string 
+}
+
+function AnimatedLines ({ delay, children } : AnimatedLinesProps) {
+  const divRef = useRef<HTMLDivElement>(null)
+  const [lines, setLines] = useState<string[]>([])
+  
+  useEffect(() => {
+    if (divRef.current) {
+      const div = divRef.current;
+      const words = children.split(' ');
+      const maxWidth = div.clientWidth;
+
+      let actualWidth = 0;
+      let tempLine = '';
+
+      const newLines = [];
+  
+      for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+  
+        if (actualWidth + word.length * 10 > maxWidth) {
+          newLines.push(tempLine);
+          tempLine = word;
+          actualWidth = word.length * 10;
+        } else {
+          tempLine += ' ' + word;
+          actualWidth += word.length * 10 + 10;
+        }
+      }
+  
+      newLines.push(tempLine);
+  
+      setLines(newLines);
+    }
+  }, [children, divRef])
+
+  console.log(lines)
+
+  return (
+    <motion.div 
+      ref={divRef}
+      className='lines'
+    >
+      {lines.map((line, index) => (
+        <motion.div
+          key={line} 
+          className="lines__line"
+        >
+          <motion.span 
+            variants={lineAnim}
+            initial="initial" 
+            animate="animate"
+            custom={delay * index}
+            className="lines__line__content"
+          >
+            {line}
+          </motion.span>
+        </motion.div>
+      ))}
+    </motion.div>
+  )
+}
+
+export { AnimatedLines }
---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import { AnimatedLines } from '@/components/AnimatedLines'
+
+function Header() {
+  return (
+    <header className='grid'>
+      <h1 className='col-1-1'>
+        <AnimatedLines delay={0}>Sam Marxz.</AnimatedLines>
+      </h1>
+      <div className='col-1-2'>
+        <h2 className='mb--medium'>
+          <AnimatedLines delay={1}>Designer && Web Developer.</AnimatedLines>
+        </h2>
+        <h3 className='mb--medium'>
+          <AnimatedLines delay={2}>Currently working as front-end developer @TCS.</AnimatedLines>
+        </h3>
+        <h4 className='mb--medium'>
+          <AnimatedLines delay={3}>Based in Brazil 🇧🇷</AnimatedLines>
+        </h4>
+      </div>
+      <a className='col-1-5' href="#contact">
+        <AnimatedLines delay={4}>Get in touch.</AnimatedLines>
+      </a>
+    </header>
+  )
+}
+
+export { Header }
